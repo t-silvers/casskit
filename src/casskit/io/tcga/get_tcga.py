6685967cfd1590d8690254c14a5fd0684f8c0d46
t-silvers <tsilvers@stanford.edu>
2022-11-08 19:44:17 -0800

    r

--- src/casskit/io/tcga/get_tcga.py
+++ src/casskit/io/tcga/get_tcga.py
@@ -1,11 +1,10 @@
-import casskit
-from casskit.io import (
-    get_tumor_purity,
-    get_ancestry_pcs,
-    get_gdc_tcga,
-)
+from ...io.tcga._gdcxena import get_gdc_tcga
+from ...io.tcga._ancestry import get_ancestry_pcs
+from ...io.tcga._tumorpurity import get_tumor_purity
+from ...config import CACHE_DIR # TEMP
 
-def get_tcga(cancer, feature, cache_dir=casskit.CACHE_DIR):
+
+def get_tcga(cancer, feature, cache_dir=CACHE_DIR):
     """Get TCGA data from local cache."""
     get_gdc_tcga(cancer, feature)
     get_tumor_purity()
