6ab497d9ec53a3254fa2ae53361097626d3e1ccf
t-silvers <tsilvers@stanford.edu>
2022-11-10 15:46:38 -0800

    r

--- src/casskit/config.py
+++ src/casskit/config.py
@@ -2,22 +2,19 @@ import os
 from pathlib import Path
 import logging
 
-
 logger = logging.getLogger(__name__)
-
 
 HOME = os.environ["HOME"]
 SCRATCH = os.environ["SCRATCH"]
 
-
-CACHE_BASE_SUBDIR = "casskit"
-CACHE_DIR_ENV_KEY = "CASSKIT_CACHE_DIR"
+_CACHE_BASE_SUBDIR = "casskit"
+_CACHE_DIR_ENV_KEY = "CASSKIT_CACHE_DIR"
 
 try:
-    CACHE_DIR = Path(os.environ[CACHE_DIR_ENV_KEY]) / ".cache" / CACHE_BASE_SUBDIR
+    CACHE_DIR = Path(os.environ[_CACHE_DIR_ENV_KEY]) / ".cache" / _CACHE_BASE_SUBDIR
 except:
-    logger.warning(f"Environment variable {CACHE_DIR_ENV_KEY} not set. Using default cache directory.")
-    CACHE_DIR = Path("~/.cache").expanduser() / ".cache" / CACHE_BASE_SUBDIR
+    logger.warning(f"Environment variable {_CACHE_DIR_ENV_KEY} not set. Using default cache directory.")
+    CACHE_DIR = Path("~/.cache").expanduser() / _CACHE_DIR_ENV_KEY
 
 CACHE_DIR.mkdir(parents=True, exist_ok=True)
 
