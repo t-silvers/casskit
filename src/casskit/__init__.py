779c0b66a4605bb363cd376556209ecb0f6151e6
t-silvers <tsilvers@stanford.edu>
2022-11-09 17:02:42 -0800

    r

--- src/casskit/__init__.py
+++ src/casskit/__init__.py
@@ -1,1 +1,26 @@
+import sys
+
+from . import io, struct
+from .config import *
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    dist_name = __name__
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+__all__ = [
+    "io", "struct", "CACHE_DIR"
+]
+
+# __path__ = __import__('pkgutil').extend_path(__path__, __name__)
