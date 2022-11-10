b48bb4616e0a18a1059be6ce1fc795e692f12f12
t-silvers <tsilvers@stanford.edu>
2022-11-09 18:04:27 -0800

    r

--- src/casskit/io/__init__.py
+++ src/casskit/io/__init__.py
@@ -1,4 +1,3 @@
-# from .ontology.get_ont import get_ont
-# from .tcga.get_tcga import build_tcga_cache, get_tcga
+from tcga.ancestry import get_ancestry_pcs
 
-# __all__ = ["build_tcga_cache", "get_tcga", "get_ont"]
+__all__ = ["get_ancestry_pcs"]
