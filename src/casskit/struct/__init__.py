dc5b1fb6af23bbfa1f21c71beb99f52e02c228ad
t-silvers <tsilvers@stanford.edu>
2022-11-09 16:26:41 -0800

    r

--- src/casskit/struct/__init__.py
+++ src/casskit/struct/__init__.py
@@ -3,12 +3,12 @@ import os
 
 os.environ["CASSKIT_CACHE_DIR"] = "/scratch/users/tsilvers/ceqtl_selection"
 
-from ._tiledb import DataDB
+# from ._tiledb import DataDB
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-__all__ = [
-    "DataDB",
-]
+# __all__ = [
+#     "DataDB",
+# ]
