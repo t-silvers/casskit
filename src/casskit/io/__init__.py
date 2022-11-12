afb1bbd8e12f33541e74c2adf1b5b38e2a5b725c
t-silvers <tsilvers@stanford.edu>
2022-11-11 23:18:23 -0800

    r

--- src/casskit/io/__init__.py
+++ src/casskit/io/__init__.py
@@ -3,18 +3,18 @@
 # from .tcga.get_tcga import build_tcga_cache, get_tcga
 
 from . import annotate
-from . import ontology
-from . import tcga
+from .ontology import get_ontology
+from .tcga import get_tcga
 
 build_ensembl_cache = annotate.build_ensembl_cache
 get_ensembl = annotate.get_ensembl
 annotate_genes = annotate.annotate_genes
 
-build_ontology_cache = ontology.get_ontology.build_ontology_cache
-get_ontology = ontology.get_ontology.get_ontology
+build_ontology_cache = get_ontology.build_ontology_cache
+get_ontology = get_ontology.get_ontology
 
-build_tcga_cache = tcga.get_tcga.build_tcga_cache
-get_tcga = tcga.get_tcga.get_tcga
+build_tcga_cache = get_tcga.build_tcga_cache
+get_tcga = get_tcga.get_tcga
 
 
 __all__ = [
