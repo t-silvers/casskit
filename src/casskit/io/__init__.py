cfac0d13fcab750247960d07c8676ff00566174a
t-silvers <tsilvers@stanford.edu>
2022-11-11 22:58:13 -0800

    r

--- src/casskit/io/__init__.py
+++ src/casskit/io/__init__.py
@@ -1,14 +1,22 @@
-from .annotate import build_ensembl_cache, get_ensembl, annotate_genes
-from .ontology.get_ontology import build_ontology_cache, get_ontology
 from .tcga.get_tcga import build_tcga_cache, get_tcga
 
 
 __all__ = [
-    "build_ensembl_cache",
-    "get_ensembl",
-    "annotate_genes",
-    "build_ontology_cache",
-    "get_ontology",
     "build_tcga_cache",
     "get_tcga",
 ]
+
+# from .annotate import build_ensembl_cache, get_ensembl, annotate_genes
+# from .ontology.get_ontology import build_ontology_cache, get_ontology
+# from .tcga.get_tcga import build_tcga_cache, get_tcga
+
+
+# __all__ = [
+#     "build_ensembl_cache",
+#     "get_ensembl",
+#     "annotate_genes",
+#     "build_ontology_cache",
+#     "get_ontology",
+#     "build_tcga_cache",
+#     "get_tcga",
+# ]
