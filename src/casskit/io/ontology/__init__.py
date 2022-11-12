b60fcc826d4f05a41e244d529534f8b3679f1827
t-silvers <tsilvers@stanford.edu>
2022-11-12 00:43:05 -0800

    r

--- src/casskit/io/ontology/__init__.py
+++ src/casskit/io/ontology/__init__.py
@@ -1,4 +1,1 @@
-from . import get as get_ontology
-
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__all__ = ["get_ontology"]
---
+++ src/casskit/models/__init__.py
@@ -0,0 +1,5 @@
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+# from . import tcga, ontology
+
+# __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+# __all__ = ["tcga", "ontology"]
