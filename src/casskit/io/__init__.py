8d5c3428af7d4dbf01eb7172af281abcfce2b8ec
t-silvers <tsilvers@stanford.edu>
2022-11-12 00:49:50 -0800

    r

--- src/casskit/io/__init__.py
+++ src/casskit/io/__init__.py
@@ -1,4 +1,10 @@
-from . import tcga, ontology
+from .ontology.get import build_ontology_cache, get_ontology
+from .tcga.get import build_tcga_cache, get_tcga
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__all__ = ["tcga", "ontology"]
+__all__ = [
+    "build_ontology_cache",
+    "get_ontology",
+    "build_tcga_cache",
+    "get_tcga"
+]
