762a488ebf6c6fe3b9e0a94e0e648b43725fc7b3
t-silvers <tsilvers@stanford.edu>
2022-11-11 23:03:13 -0800

    r

--- src/casskit/io/__init__.py
+++ src/casskit/io/__init__.py
@@ -1,14 +1,23 @@
-from casskit.io.annotate import build_ensembl_cache, get_ensembl, annotate_genes
-from casskit.io.ontology.get_ontology import build_ontology_cache, get_ontology
-from casskit.io.tcga.get_tcga import build_tcga_cache, get_tcga
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+# from .tcga.get_tcga import build_tcga_cache, get_tcga
 
 
-__all__ = [
-    "build_ensembl_cache",
-    "get_ensembl",
-    "annotate_genes",
-    "build_ontology_cache",
-    "get_ontology",
-    "build_tcga_cache",
-    "get_tcga",
-]
+# __all__ = [
+#     "build_tcga_cache",
+#     "get_tcga",
+# ]
+
+# from .annotate import build_ensembl_cache, get_ensembl, annotate_genes
+# from .ontology.get_ontology import build_ontology_cache, get_ontology
+# from .tcga.get_tcga import build_tcga_cache, get_tcga
+
+
+# __all__ = [
+#     "build_ensembl_cache",
+#     "get_ensembl",
+#     "annotate_genes",
+#     "build_ontology_cache",
+#     "get_ontology",
+#     "build_tcga_cache",
+#     "get_tcga",
+# ]
