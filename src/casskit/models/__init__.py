5e2c1478f6fe43602fab3f8680b2f9b88faadcd9
t-silvers <tsilvers@stanford.edu>
2022-11-12 00:47:51 -0800

    r

--- src/casskit/models/__init__.py
+++ src/casskit/models/__init__.py
@@ -1,4 +1,4 @@
-from . import latent
+from .latent.expression_pcs import BatchModelEPCS
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__all__ = ["latent"]
+__all__ = ["BatchModelEPCS"]
