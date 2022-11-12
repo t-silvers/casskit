a86bdae17148159f0160abada3ec4df9b2be8319
t-silvers <tsilvers@stanford.edu>
2022-11-12 00:43:54 -0800

    r

--- src/casskit/models/__init__.py
+++ src/casskit/models/__init__.py
@@ -1,5 +1,4 @@
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
-# from . import tcga, ontology
+from . import latent
 
-# __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-# __all__ = ["tcga", "ontology"]
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__all__ = ["latent"]
