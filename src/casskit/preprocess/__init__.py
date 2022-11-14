69ae62ebb375b83244899736fe7adbf0568a0358
t-silvers <tsilvers@stanford.edu>
2022-11-14 04:27:08 -0800

    r

---
+++ src/casskit/preprocess/__init__.py
@@ -0,0 +1,7 @@
+from .mutation import AggMutations
+
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__all__ = [
+    "AggMutations"
+]
--- src/casskit/preprocess/mutation.py
+++ src/casskit/preprocess/mutation.py
@@ -11,59 +11,66 @@ class AggMutations(BaseEstimator, TransformerMixin):
     def __init__(
         self,
         min_vaf: float = 0.2,
-        min_cases: int = 10
+        min_cases: int = 10,
+        binary: bool = True,
     ):
         self.min_vaf = min_vaf
         self.min_cases = min_cases
+        self.binary = binary
 
     def fit(self, X, y=None):
         return self
 
     def fit_transform(self, X, y=None):
         """Override mixin fit_transform."""
-        return self.fit(X, y).transform(X)
+        return self.transform(X)
 
     def transform(self, X, y=None):
         self.feature_names = X.columns
-        return self.cis_mut_agg(X)
-
-    def get_feature_names_out(self, input_features=None):
-        return self.feature_names
-
-    @staticmethod
-    def encode_mutation(df):
-        return df.mask(lambda df: df > 0, "mut").mask(lambda df: df == 0, "wt")
+        X_agg = self.cis_mut_agg(X)
+        X_agg.columns = ['_'.join(col) for col in X_agg.columns.values]
+        if self.binary:
+            return self.encode_mutation(X_agg)
+        return X_agg
 
     def cis_mut_agg(self, X):
         """Aggregate mutation types.
         
         Note:
-            Use VAF >+ 20% as cutoff for aggregation. This is a rule of thumb that does not have a strong theoretical basis
-            or use ploidy and tumor purity.
-                J Deng, et al. (2020) "The prognostic impact of variant allele frequency (VAF) in TP53 mutant patients with MDS: A systematic review and meta-analysis"
+            Use VAF >+ 20% as cutoff for aggregation. This is a rule of thumb
+            that does not have a strong theoretical basis or use ploidy and
+            tumor purity.
+            
+            J Deng, et al. (2020) "The prognostic impact of variant allele
+            frequency (VAF) in TP53 mutant patients with MDS: A systematic
+            review and meta-analysis"
         
             ~Use argmax(10, .1*10) as rule of thumb for observations:~
             Use 10 as rule of thumb for observations:
+                
                 Long JS (1997) "Regression Models for categorical and limited dependent variables"
                 RD Riley, et al. (2018) "Minimum sample size for developing a multivariable prediction model: PART II - binary and time-to-event outcomes"        
-        """
-        filtered_data = (X
-                         .query("dna_vaf >= @self.min_vaf")
-                         .groupby(['ensembl_gene_id','mutation_effect'])
-                         .count()
-                         .query("sample >= @self.min_cases"))
-        
-        if filtered_data.empty:
-            return filtered_data
-        
-        else:
-            second_pass = lambda df: df.loc[df.mask(lambda x: x == 'wt').count(axis=1).mask(lambda x: x < self.min_cases).index]
-            return (filtered_data
-                    .filter([])
-                    .join(X.set_index(['ensembl_gene_id', 'mutation_effect']))
-                    .reset_index()
-                    .assign(mutation_eqtl = lambda x: x['ensembl_gene_id'] + '_' + x['mutation_effect'])
-                    .pivot_table(index=['ensembl_gene_id', 'mutation_eqtl'], columns='sample', values='dna_vaf', fill_value=0)
-                    .pipe(self.encode_mutation)
-                    # Second check for minimum number of cases
-                    .pipe(second_pass))
+        """        
+        return ((X_ := self._filter_by_vaf(X))
+                .pivot_table(index=["gene", "effect"], columns="Sample_ID", values="dna_vaf")
+                .reindex(self._filter_by_cases(X_))
+                .transpose())
+
+    def _filter_by_vaf(self, X):
+        """Tally mutations."""
+        return (X
+                .query("dna_vaf >= @self.min_vaf")
+                .assign(effect = lambda x: x.pop("effect").str.split(';'))
+                .explode("effect"))
+
+    def _filter_by_cases(self, X):
+        """Tally mutations."""
+        return (X
+                .value_counts(subset=["gene", "effect"])
+                .mask(lambda x: x < self.min_cases)
+                .dropna()
+                .index)
+
+    @staticmethod
+    def encode_mutation(df):
+        return df.mask(lambda df: df > 0, "mut").mask(lambda df: df == 0, "wt")
