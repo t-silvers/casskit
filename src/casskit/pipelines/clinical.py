b4c76a3fd4516e4247046323199df9438a96c62d
t-silvers <tsilvers@stanford.edu>
2022-11-14 02:43:08 -0800

    r

---
+++ src/casskit/pipelines/clinical.py
@@ -0,0 +1,100 @@
+from typing import Dict, List, Tuple, Union
+
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+from casskit.preprocess.phenotype import ReplaceNA, TumorStageEncoder
+
+
+class ClinicalCovariates(BaseEstimator, TransformerMixin):
+
+    TCGA_VARS = [
+        "batch_number", "ethnicity.demographic", "gender.demographic",
+        "race.demographic", "age_at_diagnosis.diagnoses", "clinical_stage",
+        "tumor_stage.diagnoses", "neoplasm_histologic_grade"
+    ]
+    
+    CAT_VARS = [
+        "batch_number", "ethnicity.demographic",
+        "gender.demographic", "race.demographic",
+    ]
+
+    STAGE_VARS = ["clinical_stage", "tumor_stage.diagnoses"]
+    
+    def __init__(
+        self,
+        imp_strategy: str = "most_frequent",
+        onehot_drop: str = "if_binary",
+        min_frequency: Union[int, float] = 0.2,
+    ):
+        self.imp_strategy = imp_strategy
+        self.onehot_drop = onehot_drop
+        self.min_frequency = min_frequency
+
+    def fit(self, X, y=None):
+        self.clinical_preprocessor.fit(X[self.TCGA_VARS])
+        self.fitted_ = True
+        
+        return self
+
+    def transform(self, X):
+        if not self.fitted_:
+            self.fit(X[self.TCGA_VARS])
+
+        return self.clinical_preprocessor.transform(X[self.TCGA_VARS])
+    
+    def fit_transform(self, X, y=None):
+        """Custom fit_transform method for checks."""
+        return self.clinical_preprocessor.fit_transform(X[self.TCGA_VARS])
+    
+    @property
+    def null_values(self) -> ColumnTransformer:
+        return ColumnTransformer(transformers=[
+            ("null", ReplaceNA(),
+             make_column_selector(dtype_include=[object, np.number]))
+            ], verbose_feature_names_out=False, remainder="passthrough")
+
+    @property
+    def impute(self) -> ColumnTransformer:
+        return ColumnTransformer(transformers=[
+            ("impute", SimpleImputer(strategy=self.imp_strategy),
+             make_column_selector(dtype_include=[object, np.number]))
+            ], verbose_feature_names_out=False, remainder="passthrough")
+
+    @property
+    def one_hot(self) -> ColumnTransformer:
+        return ColumnTransformer(transformers=[
+            ("onehot", OneHotEncoder(
+                min_frequency=self.min_frequency,
+                drop=self.onehot_drop,
+                sparse_output=False    
+            ), self.CAT_VARS)
+            ], verbose_feature_names_out=False, remainder="passthrough")
+
+    @property
+    def stage_encoder(self) -> ColumnTransformer:
+        return ColumnTransformer(transformers=[
+            ("stage", TumorStageEncoder(), self.STAGE_VARS)
+            ], verbose_feature_names_out=False, remainder="passthrough")
+
+    @property
+    def singular(self) -> ColumnTransformer:
+        return ColumnTransformer(transformers=[
+            ("singular", VarianceThreshold(threshold=0),
+             make_column_selector(dtype_include=np.number))
+            ], verbose_feature_names_out=False, remainder="passthrough")
+    
+    @property
+    def clinical_preprocessor(self) -> Pipeline:
+        return Pipeline(
+            [("null_vals", self.null_values),
+             ("stage_encoder", self.stage_encoder),
+             ("impute1", self.impute),
+             ("one_hot", self.one_hot),
+             ("var_thresh", self.singular)]
+        )
---
+++ src/casskit/preprocess/phenotype.py
@@ -0,0 +1,103 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import OrdinalEncoder
+
+
+class ReplaceNA(BaseEstimator, TransformerMixin):
+    def __init__(self, replace_vals: List[str] = ["not reported"]):
+        self.replace_vals = replace_vals
+
+    def fit(self, X, y=None):
+        return self
+
+    def fit_transform(self, X, y=None):
+        """Override mixin fit_transform."""
+        return self.fit(X, y).transform(X)
+
+    def transform(self, X, y=None):
+        self.feature_names = X.columns
+        X = X.copy()
+
+        for rval in self.replace_vals:
+            mask = X.apply(lambda x: x.astype(str).str.lower()) == rval
+            X[mask] = np.nan
+
+        return X
+
+    def get_feature_names_out(self, input_features=None):
+        return self.feature_names
+
+class TumorStageEncoder(TransformerMixin, BaseEstimator):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def fit_transform(self, X, y=None):
+        """Override mixin fit_transform."""
+        return self.fit(X, y).transform(X)
+
+    def transform(self, X):
+        self.feature_names = X.columns
+        
+        if X.shape[1] > 1:
+            X = self._combine_stage_vars(X)
+
+        cleaned = (X.squeeze()
+                   .str.upper()
+                   .str.extract("STAGE ([IVX]+)", expand=False)
+                   .apply(self.rom_to_int)
+                   .to_frame()
+                   .values)
+        
+        return OrdinalEncoder(
+            handle_unknown="use_encoded_value", unknown_value=np.nan
+        ).fit_transform(cleaned)
+
+    @staticmethod
+    def _combine_stage_vars(X):
+        """If two columns, pick one with most info."""
+        selected_col = (X.apply(
+            lambda x: x.str.lower().str.contains("stage")
+        ).sum().astype(int).idxmax())
+        
+        return X[selected_col]
+   
+    @staticmethod
+    def rom_to_int(a_rom):
+        """Convert a stage roman numeral to an integer.
+
+        Adapted from https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s24.html
+        Module: Roman Numerals
+        Credit: Paul M. Winkler
+        
+        Does not handle edge cases like "I/II NOS".
+        """
+        if isinstance(a_rom, type(None)):
+            return np.nan
+        
+        if not isinstance(a_rom, type("")):
+            raise TypeError(f"expected string, got {a_rom}")
+        
+        nums = {"X":10, "V":5, "I":1}
+        a_int = 0
+        for i in range(len(a_rom)):
+            try:
+                value = nums[a_rom[i]]
+                if i+1 < len(a_rom) and nums[a_rom[i+1]] > value:
+                    a_int -= value
+                else:
+                    a_int += value
+
+            except KeyError:
+                raise ValueError(f"Input is not a valid Roman numeral: {a_rom}")
+        return a_int
+    
+    def get_feature_names_out(self, input_features=None, use_name=False):
+        if use_name:
+            return np.array(self.feature_names)
+        return np.array(["tumor_stage"])
