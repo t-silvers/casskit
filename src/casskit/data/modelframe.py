e056f71c98469dee51c3f49af07a31cce20dc3e8
t-silvers <tsilvers@stanford.edu>
2022-11-18 00:56:13 -0800

    r

--- src/casskit/data/modelframe.py
+++ src/casskit/data/modelframe.py
@@ -5,7 +5,6 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
-from casskit import dask_cluster
 from casskit.models.latent.expression_pcs import BatchModelEPCS
 from casskit.pipelines.clinical import ClinicalCovariates
 from casskit.pipelines.gtex import GTEx
@@ -85,30 +84,16 @@ class Expression:
 @dataclass
 class GeneCopyNumber:
     raw: pd.DataFrame
-    prefix: str
     preprocessing: Optional[Pipeline] = field(init=True, default=None)
     prepared: pd.DataFrame = field(init=False)
 
     def prepare(self) -> Pipeline:
-        gene_caller = GeneCaller(
-            assembly="GRCh37", nb_cpu=1, ddf_nparts=100, prefix=self.prefix
-        )
-        
-        return Pipeline([
-            ("gene copynumber", gene_caller),
-            ("to counts", ToCounts(units="log2(copy-number/2)")),
-        ])
+        return Pipeline([("", "passthrough")])
 
     def __post_init__(self):
         if self.preprocessing is None:
             self.preprocessing = self.prepare()
-    
-        if not self.preprocessing["gene copynumber"].path_cache.exists():
-            with dask_cluster(4, '32GB', 20, 1, "2:00:00") as (cluster, client):
-                prepared = self.preprocessing.fit_transform(self.raw)
-        else:
-            prepared = self.preprocessing.fit_transform(self.raw)
-
+        prepared = self.preprocessing.fit_transform(self.raw)
         self.prepared = (prepared
                          .droplevel(["Chromosome", "Start", "End"])
                          .transpose())
@@ -116,55 +101,33 @@ class GeneCopyNumber:
 @dataclass
 class BinCopyNumber:
     raw: pd.DataFrame
-    prefix: str
-    bin_cnvr_width: int = int(1E5)
     preprocessing: Optional[Pipeline] = field(init=True, default=None)
     prepared: pd.DataFrame = field(init=False)
 
     def prepare(self) -> Pipeline:
-        return BinCaller(
-            width=self.bin_cnvr_width, ddf_nparts=200, prefix=self.prefix
-        )
+        return Pipeline([("", "passthrough")])
 
     def __post_init__(self):
         if self.preprocessing is None:
             self.preprocessing = self.prepare()
-    
-        if not self.preprocessing.path_cache.exists():
-            with dask_cluster(4, '32GB', 20, 1, "2:00:00") as (cluster, client):
-                self.prepared = self.preprocessing.fit_transform(self.raw)
-        else:
-            self.prepared = self.preprocessing.fit_transform(self.raw)
+        prepared = self.preprocessing.fit_transform(self.raw)
+        self.prepared = (prepared
+                         .droplevel(["Chromosome", "Start", "End"])
+                         .transpose())
 
 @dataclass
 class CNVRCopyNumber:
     raw: pd.DataFrame
-    prefix: str
-    bin_cnvr_width: int = int(1E5)
     preprocessing: Optional[Pipeline] = field(init=True, default=None)
     prepared: pd.DataFrame = field(init=False)
 
     def prepare(self) -> Pipeline:
-        cnvr_caller = CPDcaller(
-            width=self.bin_cnvr_width, ddf_nparts=200,
-            cpd_search_method="kcpd", ragged=True, prefix=self.prefix,
-        )
-        
-        return Pipeline([
-            ("call CNVRs", cnvr_caller),
-            ("to counts", ToCounts(units="log2(copy-number/2)")),
-        ])
+        return Pipeline([("", "passthrough")])
 
     def __post_init__(self):
         if self.preprocessing is None:
             self.preprocessing = self.prepare()
-    
-        if not self.preprocessing["call CNVRs"].path_cache.exists():
-            with dask_cluster(4, '32GB', 20, 1, "2:00:00") as (cluster, client):
-                prepared = self.preprocessing.fit_transform(self.raw)
-        else:
-            prepared = self.preprocessing.fit_transform(self.raw)
-
+        prepared = self.preprocessing.fit_transform(self.raw)
         self.prepared = (prepared
                          .droplevel(["Chromosome", "Start", "End"])
                          .transpose())
