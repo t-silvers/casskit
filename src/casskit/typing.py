2d604b01fedc4061317c82b04ee8e7d8f0153e77
t-silvers <tsilvers@stanford.edu>
2022-11-20 22:00:58 -0800

    r

--- src/casskit/typing.py
+++ src/casskit/typing.py
@@ -1,8 +1,7 @@
 from typing import Dict, List, Tuple, Union
 
 import dask.dataframe as dd
-from dask.distibuted import Future
+import dask.distributed
 import pandas.DataFrame as pd
 
-
-DATAFRAME = Union[dd.DataFrame, pd.DataFrame, Future]
+DATAFRAME = Union[dd.DataFrame, pd.DataFrame, dask.distributed.Future]
