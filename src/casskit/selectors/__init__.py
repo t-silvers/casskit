856d6b5024bd16095213a952c952136e95868405
t-silvers <tsilvers@stanford.edu>
2022-11-20 21:58:30 -0800

    r

--- src/casskit/selectors/__init__.py
+++ src/casskit/selectors/__init__.py
@@ -1,9 +1,10 @@
 from .selector import ColumnSelector, LHSelector
 from .target import TransformedLHSRegressor
-
+from .utils import feature_selector
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 __all__ = [
+    "feature_selector",
     "ColumnSelector",
     "LHSelector",
     "TransformedLHSRegressor",
