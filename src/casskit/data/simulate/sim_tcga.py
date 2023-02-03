61cc1f0e5a63d7e18eb2939716956e4796a14230
t-silvers <tsilvers@stanford.edu>
2023-02-03 09:04:04 +0700

    r

--- src/casskit/data/simulate/sim_tcga.py
+++ src/casskit/data/simulate/sim_tcga.py
@@ -4,6 +4,7 @@ import warnings
 import numpy as np
 import pandas as pd
 
+# TODO: Refactor with builder class
 
 @dataclass(frozen=True)
 class SimTCGA:
@@ -40,18 +41,18 @@ class SimTCGA:
     def make_copynumber(self):
         if self.augmented is True:
             chroms = self.template.Chromosome.unique()
-            cn_skeleton = self.chrom_swap_augmented(self.N,
-                                                    self.template["sample"].unique(),
-                                                    chroms,
-                                                    self.rng
-                                                    )
+            cn_skeleton = self.chrom_swap_augmented(
+                self.N,
+                self.template["sample"].unique(),
+                chroms,
+                self.rng
+            )
 
             return (cn_skeleton
                     .assign(sample_sim = lambda x: [f"TCGA-00-{i:04}" for i in x.pop("sample_sim")])
                     .merge(self.template)
                     .drop("sample", axis=1)
-                    .rename(columns=dict(sample_sim="sample"))
-                    )
+                    .rename(columns=dict(sample_sim="sample")))
         
         else:
             n_samples = self.template["sample"].unique()
@@ -63,8 +64,7 @@ class SimTCGA:
 
             samples = self.rng.choice(self.template["sample"].unique(),
                                       size=n_sim,
-                                      replace=False
-                                      )
+                                      replace=False)
             
             return self.template.query("sample in @samples")
 
@@ -78,15 +78,12 @@ class SimTCGA:
         return (
             pd.DataFrame(rng.choice(samples,
                                     replace=True,
-                                    size=[len(groups), n_samples]
-                                    ),
-                        index=groups
-                        )
+                                    size=[len(groups), n_samples]),
+                        index=groups)
             .rename_axis("Chromosome")
             .melt(value_name="sample",
                   var_name="sample_sim",
-                  ignore_index=False
-                  )
+                  ignore_index=False)
             .reset_index()
         )
 
@@ -96,15 +93,13 @@ class SimTCGA:
                        .groupby("Chromosome")
                        .agg({"Start": "min", "End": "max"})
                        .apply(lambda x: self.rng.binomial(1, p=.5, size=self.I) * \
-                           self.rng.integers(x[0], x[1], size=self.I), axis=1)
-                       )
+                           self.rng.integers(x[0], x[1], size=self.I), axis=1))
 
             cneqtls2 = (self.template
                        .groupby("Chromosome")
                        .agg({"Start": "min", "End": "max"})
                        .apply(lambda x: self.rng.binomial(1, p=.5, size=self.I) * \
-                           self.rng.integers(x[0], x[1], size=self.I), axis=1)
-                       )
+                           self.rng.integers(x[0], x[1], size=self.I), axis=1))
 
             # combine
             cneqtls = pd.concat([cneqtls1, cneqtls2], axis=0)
@@ -126,8 +121,7 @@ class SimTCGA:
                 .assign(gene_id=lambda x: x.pop("variable").map('egene-{}'.format),
                         beta=lambda x: self.rng.normal(0, 0.5, size=len(x)),)
                 .reset_index()
-                .query("Start != 0")
-                )
+                .query("Start != 0"))
 
     def _filter_identifiability(self, grn) -> pd.DataFrame:
         """Filter GRN for identifiability based on ~h2."""
@@ -150,21 +144,6 @@ class SimTCGA:
         
         return filtered_grn
 
-    @staticmethod
-    def make_design(grn, copynumber):
-        return (grn
-                .merge(copynumber,
-                       on="Chromosome",
-                       suffixes=("", "_seg")
-                       )
-                .query("Start >= Start_seg and Start <= End")
-                .pivot_table(index=["gene_id", "Chromosome", "herit", "beta"],
-                             columns="sample",
-                             values="value",
-                             fill_value=0
-                             )
-                )
-
     def make_expression(self):
         design = self.make_design(self.grn, self.copynumber)
         expression = (design
@@ -172,8 +151,7 @@ class SimTCGA:
                       .reset_index()
                       .groupby("gene_id")
                       .apply(lambda df: np.dot(df.beta.values, df.drop("beta", axis=1).values))
-                      .apply(pd.Series)
-                      )
+                      .apply(pd.Series))
 
         # Add noise
         rng = np.random.default_rng()
@@ -186,6 +164,20 @@ class SimTCGA:
 
     def __repr__(self):
         return f"SimTCGA(N={self.N}, I={self.I}, augmented={self.augmented}, seed={self.seed})"
+
+def make_design(grn, copynumber):
+    # idx_cols = ["gene_id", "Chromosome", "herit", "beta"]
+    idx_cols = [col for col in grn.columns if 
+                col not in ["Start", "End", "sample", "value"]]
+    return (grn
+            .merge(copynumber,
+                    on="Chromosome",
+                    suffixes=("", "_seg"))
+            .query("Start >= Start_seg and Start <= End")
+            .pivot_table(index=idx_cols,
+                         columns="sample",
+                         values="value",
+                         fill_value=0))
 
 def simulate_tcga(
     template: pd.DataFrame,
@@ -200,5 +192,4 @@ def simulate_tcga(
                    I=I,
                    seed=seed,
                    augmented=augmented,
-                   min_h2=min_h2,
-                   )
+                   min_h2=min_h2)
